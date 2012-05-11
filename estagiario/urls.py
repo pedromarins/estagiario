86c7dca0dc2d708b8edfcd33af1cb911e90f8628
Pedro Marins <contato@pedromarins.com>
2012-05-11 11:38:52 -0300

    página de cadastro da vaga com direct_to_template

--- estagiario/urls.py
+++ estagiario/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     #url(r'^area/(?P<area_slug>\S+)$', 'views.view_area', name='view_area'),                        
     url(r'^publicar-vaga/$',      'internships.views.add_internship',       name='add_internship'),   
     url(r'^contato/$',      'core.views.contato',       name='envelope-contact'),   
+    url( r'^anunciar-vaga/$', direct_to_template, { 'template': 'companies/companies.html' }),
     
     url(r'', include('social_auth.urls')),
     
